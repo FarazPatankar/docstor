a51a6fe0528b1516960c685463c5977b8acea1c5
Faraz Patankar <farazpatankar@gmail.com>
2016-06-07 13:17:50 +0530

    set up regex to validate ruby docs search

--- routes/docs.js
+++ routes/docs.js
@@ -2,8 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 router.post('/', function (req, res) {
-	console.log(req);
-  	res.json('HI');
+	// console.log(req.body.text);
+
+	var text = req.body.text
+
+	if (text.match(/[a-zA-Z]+([#]|[:]{2})[a-zA-Z_]+/)){
+		res.json('Good job, kid!')
+	} else {
+	  	res.json('YOU ARE WRONG!');
+	}
 });
 
 module.exports = router;
