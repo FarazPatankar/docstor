22c92e92d3935f3f873dd35a1fb5116014040adb
Faraz Patankar <farazpatankar@gmail.com>
2016-06-08 04:02:55 +0530

    add separate helper file to store helper functions

---
+++ routes/lib/helper.js
@@ -0,0 +1,6 @@
+module.exports = {
+    //Function that returns the capitalized form of the string
+    capitalize: function(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+}
