3cf746ddb4fac2755e67036b3d37310819b945c4
Faraz Patankar <farazpatankar@gmail.com>
2016-06-09 02:29:42 +0530

    add helper function to reverse string

--- routes/lib/helper.js
+++ routes/lib/helper.js
@@ -2,5 +2,14 @@ module.exports = {
     //Function that returns the capitalized form of the string
     capitalize: function(string) {
         return string.charAt(0).toUpperCase() + string.slice(1);
+    },
+    reverseString: function(str) {
+        var splitString = str.split("");
+
+        var reverseArray = splitString.reverse();
+
+        var joinArray = reverseArray.join("");
+
+        return joinArray;
     }
 }
